754c2ecbccf0c68a3ca565e03f7dbffe487f06db
Alan Smith <commit@alanwsmith.com>
2024-10-09 20:19:52 -0400

    Finished putting new code in place

--- docs/scripts/theme.js
+++ docs/scripts/theme.js
@@ -89,7 +89,7 @@ function makeContentVisible() {
 
 document.addEventListener("DOMContentLoaded", () => {
   addschemeSwitchers()
-  //duplicateDarkStyles()
+  duplicateDarkStyles()
   updateScheme()
   makeContentVisible()
 
